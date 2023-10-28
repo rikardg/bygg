06641f514c0bc396dd3a3ced030226db47b69677
Rikard Gillemyr <rikard.gillemyr@gmail.com>
2023-10-28 22:23:43 +0200

    Put the default action first in --list output
    For output style B, also suffix it with " (default)".

--- src/bygg/main.py
+++ src/bygg/main.py
@@ -191,6 +191,22 @@ def list_actions(ctx: ByggContext, configuration: ByggFile | None) -> bool:
     terminal_cols, terminal_rows = shutil.get_terminal_size()
     output = [f"{TS.BOLD}Available actions:{TS.RESET}"]
 
+    sorted_actions = sorted(entrypoints, key=lambda x: x.name)
+    default_action_name = (
+        configuration.settings.default_action if configuration else None
+    )
+
+    if default_action_name:
+        default_action_list = [
+            x for x in sorted_actions if x.name == default_action_name
+        ]
+
+        if default_action_list:
+            default_action = default_action_list[0]
+            default_action_name = default_action.name
+            sorted_actions.remove(default_action)
+            sorted_actions.insert(0, default_action)
+
     if list_actions_style == "A":
         output.append("")
         section_indent = 0
@@ -198,7 +214,7 @@ def list_actions(ctx: ByggContext, configuration: ByggFile | None) -> bool:
         max_name_width = max([len(x.name) for x in entrypoints])
         width = min(terminal_cols, 80)
         subsequent_indent = " " * (section_indent + max_name_width + len(separator))
-        for action in sorted(entrypoints, key=lambda x: x.name):
+        for action in sorted_actions:
             description = f"{TS.BOLD}{action.name: <{max_name_width}}{TS.RESET}{separator}{action.description if action.description else 'No description'}"
             output.extend(
                 textwrap.wrap(
@@ -212,8 +228,11 @@ def list_actions(ctx: ByggContext, configuration: ByggFile | None) -> bool:
 
     if list_actions_style == "B":
         output.append("")
-        for action in sorted(entrypoints, key=lambda x: x.name):
-            output.append(f"{TS.BOLD}{action.name}{TS.RESET}")
+        for action in sorted_actions:
+            default_action_suffix = (
+                " (default)" if action.name == default_action_name else ""
+            )
+            output.append(f"{TS.BOLD}{action.name}{default_action_suffix}{TS.RESET}")
             output.append(
                 textwrap.fill(
                     action.description if action.description else "No description",
