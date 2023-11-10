f2aa6fe7cef942b9808db9b59f6afa20dfbfd65a
Rikard Gillemyr <rikard.gillemyr@gmail.com>
2023-11-11 00:06:33 +0100

    Add tests for --list and --tree
    Add tests for the --list and --tree functionality using pytest, and
    check the output using snapshots.
    
    Include notes for tests that are known to fail or pass when they
    shouldn't.
    
    Add a fixture for setting up a clean copy of the repo files a temporary
    directory, and run the tests there. This is needed in case they behave
    differently the first and subsequent times they are run.

---
+++ tests/test_main.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from pathlib import Path
+import subprocess
+
+import pytest
+
+
+@dataclass
+class ExampleParameters:
+    name: str
+    list_rc: int = 0
+
+
+# TODO exceptions below indicate missing functionality
+examples = [
+    ExampleParameters("checks"),
+    ExampleParameters("environments"),
+    ExampleParameters("failing_jobs"),
+    ExampleParameters("only_python"),  # TODO: should have list_rc=1
+    ExampleParameters("parametric"),
+    ExampleParameters("taskrunner"),
+    ExampleParameters("trivial", list_rc=1),  # TODO: should have list_rc=0
+]
+
+examples_dir = Path("examples")
+
+
+@pytest.mark.parametrize("example", examples, ids=lambda x: x.name)
+def test_list(snapshot, clean_bygg_tree, example):
+    process = subprocess.run(
+        ["bygg", "--list"],
+        cwd=clean_bygg_tree / examples_dir / example.name,
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert process.returncode == example.list_rc
+    assert process.stdout == snapshot
+
+
+@pytest.mark.parametrize("example", examples, ids=lambda x: x.name)
+def test_list_directory(snapshot, clean_bygg_tree, example):
+    process = subprocess.run(
+        ["bygg", "--list", "-C", examples_dir / example.name],
+        cwd=clean_bygg_tree,
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert process.returncode == example.list_rc
+    assert process.stdout == snapshot
+
+
+@pytest.mark.parametrize("example", examples, ids=lambda x: x.name)
+def test_tree(snapshot, clean_bygg_tree, example):
+    process = subprocess.run(
+        ["bygg", "--tree"],
+        cwd=clean_bygg_tree / examples_dir / example.name,
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert process.returncode == 0
+    assert process.stdout == snapshot
+
+
+@pytest.mark.parametrize("example", examples, ids=lambda x: x.name)
+def test_tree_directory(snapshot, clean_bygg_tree, example):
+    process = subprocess.run(
+        ["bygg", "--tree", "-C", examples_dir / example.name],
+        cwd=clean_bygg_tree,
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert process.returncode == 0
+    assert process.stdout == snapshot
