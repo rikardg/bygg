572b79bab07b8e9e10e2526a1fc1494b00106e40
Rikard Gillemyr <rikard.gillemyr@gmail.com>
2023-11-26 22:29:20 +0100

    Port mypy.sh to Python
    Also adjust arguments to mypy, including adding special arguments for
    the examples/environments directory.

---
+++ mypy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+from bygg.output import output_error
+
+from src.bygg.output import TerminalStyle as TS
+from src.bygg.system_helpers import call
+
+directories = [
+    Path("src/bygg"),
+    *{p.parent for p in Path("examples").glob("*/*.py")},
+]
+mypy = Path(".venv/bin/mypy")
+extra_arguments = {
+    # We want to type check without having to have all the environments set up.
+    Path("examples/environments/"): "--ignore-missing-imports",
+}
+
+exit_code = 0
+
+for directory in directories:
+    print(f"\n{TS.BOLD}=== Running mypy in {directory} ==={TS.RESET}")
+    s = call(
+        f"{mypy} --no-warn-no-return --pretty {extra_arguments.get(directory, '')} {directory}"
+    )
+    if not s:
+        output_error(f"Failed to run mypy in {directory}")
+        exit_code |= s
+
+exit(exit_code)
--- mypy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for d in src/bygg example/ examples/* ; do
-  echo
-  echo "=== Running mypy in $d ==="
-  if [ -d "$d" ] ; then
-    .venv/bin/mypy "$d"
-  fi
-done
