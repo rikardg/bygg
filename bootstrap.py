7626335d80449ce2f742366f99f57da59a02d154
Rikard Gillemyr <rikard.gillemyr@gmail.com>
2024-03-30 15:18:12 +0100

    Use uv if available
    Use uv for environment creation and for installing requirements in
    bootstrap.py if it is available. This gives much faster setup times.

--- bootstrap.py
+++ bootstrap.py
@@ -8,25 +8,29 @@ from src.bygg.system_helpers import call
 
 venv = Path(".venv")
 
+uv_bin = shutil.which("uv")
+venv_command = "python3 -m venv" if not uv_bin else f"{uv_bin} venv"
+pip_install_command = ".venv/bin/pip install" if not uv_bin else f"{uv_bin} pip install"
+
 # Always remove existing venv, to make sure requirements are correctly installed.
 if venv.exists():
     print(f"Removing existing {venv} directory")
     shutil.rmtree(venv)
 
 print("Creating Python virtualenv")
-s = call(f"python3 -m venv {venv}")
+s = call(f"{venv_command} {venv}")
 if not s or not venv.exists():
     print("Failed to create Python virtualenv")
     sys.exit(s)
 
 print("Installing requirements:")
-s = call(".venv/bin/pip install -r requirements.txt -r requirements-dev.txt")
+s = call(f"{pip_install_command} -r requirements.txt -r requirements-dev.txt")
 if not s:
     print("Failed to install requirements")
     sys.exit(s)
 
 print("Installing myself:")
-s = call(".venv/bin/pip install -e .")
+s = call(f"{pip_install_command} -e .")
 if not s:
     print("Failed to install Bygg")
     sys.exit(s)
