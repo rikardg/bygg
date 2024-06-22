c892c611d706400c3e2ee3b64db59dd64c7b9a0f
Rikard Gillemyr <rikard.gillemyr@gmail.com>
2024-06-22 18:30:40 +0200

    Have --tree deal with non-existing entrypoints
    Return False when encountering a non-existing entrypoint and not running
    in a subprocess.
    
    Add tests.

--- tests/test_main.py
+++ tests/test_main.py
@@ -74,6 +74,22 @@ def test_tree_directory(snapshot, clean_bygg_tree, example):
     assert process.stdout == snapshot
 
 
+@pytest.mark.parametrize("example", examples, ids=lambda x: x.name)
+def test_tree_non_existing_action(snapshot, clean_bygg_tree, example):
+    process = subprocess.run(
+        ["bygg", "--tree", "no_such_action"],
+        cwd=clean_bygg_tree / examples_dir / example.name,
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert process.returncode == 1
+    assert process.stdout == snapshot
+
+
+# TODO more tests:
+# --tree ACTION (one and several existing )
+
+
 def test_dump_schema(snapshot):
     process = subprocess.run(
         ["bygg", "--dump-schema"],
