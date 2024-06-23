6d181fbf9e2579cebed366ecf80b51c222d4f404
Rikard Gillemyr <rikard.gillemyr@gmail.com>
2024-06-23 20:57:55 +0200

    Fix input list bug in action_set
    The input list of the Actions created by the action_set decorator would
    be empty unless extra_inputs was also passed in.
    
    Add a test for this.
    
    Add a testcases directory intended to hold tests for small end-to-end
    testcases like this, run with pytest.

---
+++ tests/test_testcases.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass
+from pathlib import Path
+import subprocess
+
+import pytest
+
+
+@dataclass
+class TestcaseParameters:
+    __test__ = False
+    name: str
+    actions: list[str]
+
+
+testcases_dir = Path("testcases")
+
+testcases: list[TestcaseParameters] = [
+    TestcaseParameters("action_set", ["testfiles_static"]),
+]
+
+
+filetree_ignore = (
+    "__pycache__",
+    ".bygg",
+)
+
+
+def filetree(path: str):
+    return sorted(
+        [
+            str(p.relative_to(path))
+            for p in Path(path).rglob("**/*")
+            if not any(sub in str(p) for sub in filetree_ignore)
+        ]
+    )
+
+
+@pytest.mark.parametrize("testcase", testcases, ids=lambda x: x.name)
+def test_list(snapshot, clean_bygg_tree, testcase):
+    example_path = clean_bygg_tree / testcases_dir / testcase.name
+
+    # assert filetree_before == snapshot
+
+    process = subprocess.run(
+        ["bygg", "--list"],
+        cwd=clean_bygg_tree / testcases_dir / testcase.name,
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert process.returncode == 0
+    assert filetree(example_path) == snapshot
+
+    for action in testcase.actions:
+        process = subprocess.run(
+            ["bygg", action],
+            cwd=clean_bygg_tree / testcases_dir / testcase.name,
+            capture_output=True,
+            encoding="utf-8",
+        )
+        assert process.returncode == 0
+        assert filetree(example_path) == snapshot
+
+        process = subprocess.run(
+            ["bygg", action, "--clean"],
+            cwd=clean_bygg_tree / testcases_dir / testcase.name,
+            capture_output=True,
+            encoding="utf-8",
+        )
+        assert process.returncode == 0
+        assert filetree(example_path) == snapshot
