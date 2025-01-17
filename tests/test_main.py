rikardg/bygg

commit e2ff1967149741bc5b0f1bf53660ae754c1e80c6

Rewrite dispatching mechanism
The dispatcher code has grown organically since the beginning of the
project and has been patched along the way to include new features. This
has made it increasingly harder to maintain and add new features to.

This change is a rewrite of the dispatching mechanism. This includes the
way that environments are loaded and how actions are executed in them as
well as in the top-level and subprocess dispatch.

Also, there is now no longer a default environment. Actions declared in
the static configuration are executed in the ambient environment unless
they also have an environment declared.

tests/__snapshots__/test_main.ambr
@@ -23,7 +23,7 @@
           ]
         },
         "title": "ActionItem",
-        "description": "This is a representation of the Action class used for deserialising from YAML.\nThe name of the action is the key in the dictionary in the config file.\n\nParameters\n----------\ndescription : str, optional\n    A description of the action. Used in e.g. action listings. Default is None.\nmessage : str, optional\n    A message to print when the action is executed. Default is None.\ninputs : list of str, optional\n    A list of files that are used as input to the action. Default is None.\noutputs : list of str, optional\n    A list of files that are generated by the action. Default is None.\ndependencies : list of str, optional\n    A list of actions that must be executed before this action. Default is None.\nis_entrypoint : bool, optional\n    Whether this action is an entrypoint. Entrypoints are actions that can be\n    executed directly from the command line. If not set, this is treated as true by\n    default in Byggfile.yml and false when used from Python. Default is None.\nenvironment : str, optional\n    The environment for the action. Default is \"default\".\nshell : str, optional\n    The shell command to execute when the action is run. Default is None."
+        "description": "This is a representation of the Action class used for deserialising from YAML.\nThe name of the action is the key in the dictionary in the config file.\n\nParameters\n----------\ndescription : str, optional\n    A description of the action. Used in e.g. action listings. Default is None.\nmessage : str, optional\n    A message to print when the action is executed. Default is None.\ninputs : list of str, optional\n    A list of files that are used as input to the action. Default is None.\noutputs : list of str, optional\n    A list of files that are generated by the action. Default is None.\ndependencies : list of str, optional\n    A list of actions that must be executed before this action. Default is None.\nis_entrypoint : bool, optional\n    Whether this action is an entrypoint. Entrypoints are actions that can be\n    executed directly from the command line. If not set, this is treated as true by\n    default in Byggfile.yml and false when used from Python. Default is None.\nenvironment : str, optional\n    The environment for the action. Default is to run in the ambient environment.\nshell : str, optional\n    The shell command to execute when the action is run. Default is None."
       },
       "settings": {
         "allOf": [
@@ -136,7 +136,7 @@
                 "type": "null"
               }
             ],
-            "default": "default"
+            "default": "_BYGG_DEFAULT_NULL_ENVIRONMENT"
           },
           "shell": {
             "anyOf": [
@@ -151,7 +151,7 @@
           }
         },
         "additionalProperties": false,
-        "description": "This is a representation of the Action class used for deserialising from YAML.\nThe name of the action is the key in the dictionary in the config file.\n\nParameters\n----------\ndescription : str, optional\n    A description of the action. Used in e.g. action listings. Default is None.\nmessage : str, optional\n    A message to print when the action is executed. Default is None.\ninputs : list of str, optional\n    A list of files that are used as input to the action. Default is None.\noutputs : list of str, optional\n    A list of files that are generated by the action. Default is None.\ndependencies : list of str, optional\n    A list of actions that must be executed before this action. Default is None.\nis_entrypoint : bool, optional\n    Whether this action is an entrypoint. Entrypoints are actions that can be\n    executed directly from the command line. If not set, this is treated as true by\n    default in Byggfile.yml and false when used from Python. Default is None.\nenvironment : str, optional\n    The environment for the action. Default is \"default\".\nshell : str, optional\n    The shell command to execute when the action is run. Default is None."
+        "description": "This is a representation of the Action class used for deserialising from YAML.\nThe name of the action is the key in the dictionary in the config file.\n\nParameters\n----------\ndescription : str, optional\n    A description of the action. Used in e.g. action listings. Default is None.\nmessage : str, optional\n    A message to print when the action is executed. Default is None.\ninputs : list of str, optional\n    A list of files that are used as input to the action. Default is None.\noutputs : list of str, optional\n    A list of files that are generated by the action. Default is None.\ndependencies : list of str, optional\n    A list of actions that must be executed before this action. Default is None.\nis_entrypoint : bool, optional\n    Whether this action is an entrypoint. Entrypoints are actions that can be\n    executed directly from the command line. If not set, this is treated as true by\n    default in Byggfile.yml and false when used from Python. Default is None.\nenvironment : str, optional\n    The environment for the action. Default is to run in the ambient environment.\nshell : str, optional\n    The shell command to execute when the action is run. Default is None."
       },
       "Settings": {
         "type": "object",
@@ -446,16 +446,18 @@
   '''
   🛈 Setting up environment "Environment 1" in .venv1
   🛈 Setting up environment in .venv2
-  🛈 Setting up environment "Default environment" in .venv
+  🛈 Setting up environment "Base environment" in .venv
   Available actions:
   
-  ~~ Default environment ~~
+  ~~ No environment ~~
   
   clean_environments
       Clean all environments
   
+  ~~ Base environment ~~
+  
   default_action (default)
-      The default action. Runs in the default environment.
+      The default action. Runs in the base environment.
   
   ~~ Environment 1 ~~
   
@@ -512,11 +514,11 @@
   '''
   Available actions:
   
-  sort (default)
-      Sort all test files. Also sets up test files if needed.
-  
   setup
       Set up test files.
+  
+  sort (default)
+      Sort all test files. Also sets up test files if needed.
   
   
   '''
@@ -524,9 +526,6 @@
 # name: test_list[taskrunner]
   '''
   Available actions:
-  
-  touch a file (default)
-      `touch foo`
   
   do something from TOML
       A test action in a TOML file
@@ -542,6 +541,9 @@
   
   shorthand_action_yaml
       `ls -l`
+  
+  touch a file (default)
+      `touch foo`
   
   
   '''
@@ -586,16 +588,18 @@
   🛈 Entering directory 'examples/environments'
   🛈 Setting up environment "Environment 1" in .venv1
   🛈 Setting up environment in .venv2
-  🛈 Setting up environment "Default environment" in .venv
+  🛈 Setting up environment "Base environment" in .venv
   Available actions:
   
-  ~~ Default environment ~~
+  ~~ No environment ~~
   
   clean_environments
       Clean all environments
   
+  ~~ Base environment ~~
+  
   default_action (default)
-      The default action. Runs in the default environment.
+      The default action. Runs in the base environment.
   
   ~~ Environment 1 ~~
   
@@ -655,11 +659,11 @@
   🛈 Entering directory 'examples/parametric'
   Available actions:
   
-  sort (default)
-      Sort all test files. Also sets up test files if needed.
-  
   setup
       Set up test files.
+  
+  sort (default)
+      Sort all test files. Also sets up test files if needed.
   
   
   '''
@@ -668,9 +672,6 @@
   '''
   🛈 Entering directory 'examples/taskrunner'
   Available actions:
-  
-  touch a file (default)
-      `touch foo`
   
   do something from TOML
       A test action in a TOML file
@@ -686,6 +687,9 @@
   
   shorthand_action_yaml
       `ls -l`
+  
+  touch a file (default)
+      `touch foo`
   
   
   '''
@@ -704,8 +708,7 @@
 # name: test_non_existing_action[checks]
   '''
   🛈 Entering directory 'examples/checks'
-  🛈 Building action 'no_such_action':
-  Error: Action ''no_such_action'' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
@@ -714,48 +717,43 @@
   🛈 Entering directory 'examples/environments'
   🛈 Setting up environment "Environment 1" in .venv1
   🛈 Setting up environment in .venv2
-  🛈 Setting up environment "Default environment" in .venv
-  Action 'no_such_action' not found in any environment.
+  🛈 Setting up environment "Base environment" in .venv
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_non_existing_action[failing_jobs]
   '''
   🛈 Entering directory 'examples/failing_jobs'
-  🛈 Building action 'no_such_action':
-  Error: Action ''no_such_action'' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_non_existing_action[only_python]
   '''
   🛈 Entering directory 'examples/only_python'
-  🛈 Building action 'no_such_action':
-  Error: Action ''no_such_action'' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_non_existing_action[parametric]
   '''
   🛈 Entering directory 'examples/parametric'
-  🛈 Building action 'no_such_action':
-  Error: Action ''no_such_action'' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_non_existing_action[taskrunner]
   '''
   🛈 Entering directory 'examples/taskrunner'
-  🛈 Building action 'no_such_action':
-  Error: Action ''no_such_action'' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_non_existing_action[trivial]
   '''
   🛈 Entering directory 'examples/trivial'
-  🛈 Building action 'no_such_action':
-  Error: Action ''no_such_action'' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
@@ -821,7 +819,7 @@
   🛈 Removing environments
   🛈 Removing venv env1 at .venv1
   🛈 Removing venv env2 at .venv2
-  🛈 Removing venv default at .venv
+  🛈 Removing venv base at .venv
   
   '''
 # ---
@@ -874,7 +872,7 @@
   🛈 Removing environments
   🛈 Removing venv env1 at .venv1
   🛈 Removing venv env2 at .venv2
-  🛈 Removing venv default at .venv
+  🛈 Removing venv base at .venv
   🛈 Removing cache
   
   '''
@@ -939,7 +937,7 @@
   '''
   🛈 Setting up environment "Environment 1" in .venv1
   🛈 Setting up environment in .venv2
-  🛈 Setting up environment "Default environment" in .venv
+  🛈 Setting up environment "Base environment" in .venv
   
   default_action
   └── default_action_python
@@ -959,8 +957,8 @@
 # ---
 # name: test_tree[only_python]
   '''
-  No actions specified and no default action is defined.
-  
+  The --tree command requires at least one action to be specified.
+  No actions were specified and no default action is defined.
   Available actions:
   
   hello
@@ -1322,7 +1320,7 @@
   🛈 Entering directory 'examples/environments'
   🛈 Setting up environment "Environment 1" in .venv1
   🛈 Setting up environment in .venv2
-  🛈 Setting up environment "Default environment" in .venv
+  🛈 Setting up environment "Base environment" in .venv
   
   default_action
   └── default_action_python
@@ -1344,8 +1342,8 @@
 # name: test_tree_directory[only_python]
   '''
   🛈 Entering directory 'examples/only_python'
-  No actions specified and no default action is defined.
-  
+  The --tree command requires at least one action to be specified.
+  No actions were specified and no default action is defined.
   Available actions:
   
   hello
@@ -1690,7 +1688,7 @@
 # ---
 # name: test_tree_non_existing_action[checks]
   '''
-  Error: Action 'no_such_action' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
@@ -1698,38 +1696,38 @@
   '''
   🛈 Setting up environment "Environment 1" in .venv1
   🛈 Setting up environment in .venv2
-  🛈 Setting up environment "Default environment" in .venv
-  Action 'no_such_action' not found in any environment.
+  🛈 Setting up environment "Base environment" in .venv
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_tree_non_existing_action[failing_jobs]
   '''
-  Error: Action 'no_such_action' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_tree_non_existing_action[only_python]
   '''
-  Error: Action 'no_such_action' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_tree_non_existing_action[parametric]
   '''
-  Error: Action 'no_such_action' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_tree_non_existing_action[taskrunner]
   '''
-  Error: Action 'no_such_action' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---
 # name: test_tree_non_existing_action[trivial]
   '''
-  Error: Action 'no_such_action' not found.
+  Error: The following action could not be found: 'no_such_action'.
   
   '''
 # ---

examples/environments/Byggfile.yml
@@ -9,9 +9,10 @@ actions:
       rm -Rf .venv .venv1 .venv2
 
   default_action:
-    description: "The default action. Runs in the default environment."
-    message: "Running in default env"
+    description: "The default action. Runs in the base environment."
+    message: "Running in base env"
     dependencies: ["default_action_python"]
+    environment: base
 
   action1:
     description: "Action that runs in env1."
@@ -60,8 +61,8 @@ environments:
         .venv2/bin/pip install -e ../../
       fi
 
-  default:
-    name: "Default environment"
+  base:
+    name: "Base environment"
     byggfile: "Byggfile.py"
     inputs:
       - requirements.txt

src/bygg/cmd/apply_configuration.py
@@ -1,104 +1,0 @@
-import os
-from pathlib import Path
-import sys
-import textwrap
-from typing import TYPE_CHECKING
-
-from bygg.cmd.configuration import PYTHON_INPUTFILE, Byggfile, Environment
-from bygg.cmd.environments import setup_environment
-from bygg.core.action import Action
-from bygg.logging import logger
-from bygg.util import create_shell_command
-
-
-def should_restart_with(environment: Environment) -> str | None:
-    venv_bin_path = Path(environment.venv_directory) / "bin"
-
-    # # TODO: we could check here if the new venv is using the same Python binary that
-    # # we're already running under, and if so just activate the venv.
-
-    is_relative = Path(sys.executable).is_relative_to(venv_bin_path.resolve())
-    if not is_relative:
-        return str((venv_bin_path / "bygg"))
-    return None
-
-
-def register_actions_from_configuration(
-    configuration: Byggfile, is_restarted_with_env: str | None
-):
-    logger.info(
-        "Registering actions from configuration for '%s'", is_restarted_with_env
-    )
-    for action_name, action in configuration.actions.items():
-        logger.debug("Action '%s'", action_name)
-        if TYPE_CHECKING:
-            assert not isinstance(action, str)
-
-        if is_restarted_with_env and action.environment != is_restarted_with_env:
-            logger.debug(
-                "Skipping action '%s' for environment '%s', it belongs to '%s'",
-                action_name,
-                is_restarted_with_env,
-                action.environment,
-            )
-            continue
-
-        shell_command = (
-            create_shell_command(action.shell, action.message) if action.shell else None
-        )
-        Action(
-            action_name,
-            # Use the shell command as fallback for the description
-            description=action.description or f"`{action.shell}`",
-            is_entrypoint=bool(action.is_entrypoint),
-            inputs=action.inputs,
-            outputs=action.outputs,
-            dependencies=action.dependencies,
-            command=shell_command,
-        )
-
-
-def apply_configuration(
-    configuration: Byggfile,
-    environment_name: str | None,
-    is_restarted_with_env: str | None,
-) -> str | None:
-    """Returns the path of the bygg install to restart with if a restart is needed."""
-
-    environment = (
-        configuration.environments[environment_name]
-        if environment_name and environment_name in configuration.environments
-        else None
-    )
-
-    # Check if we need to restart to run in a different environment:
-
-    if not is_restarted_with_env and environment:
-        setup_environment(environment)
-
-        restart_with = should_restart_with(environment)
-        if restart_with is not None:
-            return restart_with
-
-    # Now set up the actions for the current environment:
-    register_actions_from_configuration(configuration, is_restarted_with_env)
-
-    # Evaluate the Python build file:
-
-    python_build_file = environment.byggfile if environment else PYTHON_INPUTFILE
-    load_python_build_file(python_build_file)
-    return None
-
-
-def load_python_build_file(build_file: str):
-    # modify load path to make the current directory importable
-    preamble = """\
-        import os
-        import sys
-        sys.path.insert(0, str(os.path.realpath('.')))
-
-        """
-
-    if os.path.isfile(build_file):
-        with open(build_file, "r") as f:
-            exec(textwrap.dedent(preamble) + f.read(), globals())

src/bygg/cmd/completions.py
@@ -163,9 +163,9 @@ class EntrypointCompleter(BaseCompleter):
     ):
         import textwrap
 
-        from bygg.cmd.dispatcher import dispatcher
+        from bygg.cmd.dispatcher import parent_dispatcher
 
-        subprocess_data = dispatcher(parser, parsed_args)
+        subprocess_data = parent_dispatcher(parser, parsed_args)
         if not subprocess_data:
             return {}
 

src/bygg/cmd/configuration.py
@@ -14,7 +14,7 @@ PYTHON_INPUTFILE = "Byggfile.py"
 TOML_INPUTFILE = "Byggfile.toml"
 YAML_INPUTFILE = "Byggfile.yml"
 
-DEFAULT_ENVIRONMENT_NAME = "default"
+DEFAULT_ENVIRONMENT_NAME = "_BYGG_DEFAULT_NULL_ENVIRONMENT"
 
 
 @dataclasses.dataclass
@@ -35,7 +35,7 @@ class Settings:
 
 @dataclasses.dataclass
 class ActionItem:
-    __doc__ = f"""
+    """
     This is a representation of the Action class used for deserialising from YAML.
     The name of the action is the key in the dictionary in the config file.
 
@@ -56,7 +56,7 @@ class ActionItem:
         executed directly from the command line. If not set, this is treated as true by
         default in Byggfile.yml and false when used from Python. Default is None.
     environment : str, optional
-        The environment for the action. Default is "{DEFAULT_ENVIRONMENT_NAME}".
+        The environment for the action. Default is to run in the ambient environment.
     shell : str, optional
         The shell command to execute when the action is run. Default is None.
     """

src/bygg/cmd/datastructures.py
@@ -1,6 +1,8 @@
+import argparse
 from dataclasses import dataclass, field
 from typing import Optional
 
+from bygg.cmd.argument_parsing import ByggNamespace
 from bygg.cmd.configuration import Byggfile
 from bygg.core.runner import ProcessRunner
 from bygg.core.scheduler import Scheduler
@@ -29,6 +31,7 @@ class SubProcessIpcData:
     found_actions: set[str] = field(default_factory=set)
     list: Optional[SubProcessIpcDataList] = None
     tree: Optional[SubProcessIpcDataTree] = None
+    return_code: int = 0
 
 
 @dataclass
@@ -38,7 +41,10 @@ class ByggContext:
     runner: ProcessRunner
     scheduler: Scheduler
     configuration: Byggfile
-    ipc_data: Optional[SubProcessIpcData] = None
+    parser: argparse.ArgumentParser
+    args_namespace: argparse.Namespace
+    bygg_namespace: ByggNamespace
+    ipc_data: SubProcessIpcData
 
 
 @dataclass

src/bygg/cmd/environments.py
@@ -1,11 +1,23 @@
+import os
 from pathlib import Path
 import shutil
 import subprocess
 import sys
+import textwrap
+from typing import TYPE_CHECKING
 
-from bygg.cmd.configuration import Byggfile, Environment
+from bygg.cmd.configuration import (
+    DEFAULT_ENVIRONMENT_NAME,
+    PYTHON_INPUTFILE,
+    Byggfile,
+    Environment,
+)
+from bygg.cmd.datastructures import ByggContext
+from bygg.core.action import Action
 from bygg.core.digest import calculate_string_digest
+from bygg.logging import logger
 from bygg.output.output import output_error, output_info, output_plain
+from bygg.util import create_shell_command
 
 
 def calculate_environment_hash(environment: Environment) -> str:
@@ -16,6 +28,20 @@ def calculate_environment_hash(environment: Environment) -> str:
             with open(input, "r") as f:
                 requirements += f.readlines()
     return calculate_string_digest(" ".join(requirements))
+
+
+def should_restart_with(environment: Environment) -> str | None:
+    """Returns the path to the bygg file to restart with if it's not the one in the
+    already loaded environment; in that case returns None."""
+    venv_bin_path = Path(environment.venv_directory) / "bin"
+
+    # # TODO: we could check here if the new venv is using the same Python binary that
+    # # we're already running under, and if so just activate the venv.
+
+    is_relative = Path(sys.executable).is_relative_to(venv_bin_path.resolve())
+    if not is_relative:
+        return str((venv_bin_path / "bygg"))
+    return None
 
 
 def setup_environment(environment: Environment):
@@ -69,3 +95,72 @@ def remove_environments(configuration: Byggfile) -> list[str]:
             shutil.rmtree(venv_path)
             removed_environments.append(name)
     return removed_environments
+
+
+def load_environment(ctx: ByggContext, environment_name: str):
+    environment = ctx.configuration.environments.get(environment_name, None)
+
+    # Now set up the actions for the current environment:
+    register_actions_from_configuration(ctx.configuration, environment_name)
+
+    # Evaluate the Python build file:
+
+    # If the default Python byggfile is used in an environment, don't load it by default
+    if environment_name == DEFAULT_ENVIRONMENT_NAME and PYTHON_INPUTFILE in {
+        bf.byggfile for bf in ctx.configuration.environments.values()
+    }:
+        return None
+
+    python_build_file = environment.byggfile if environment else PYTHON_INPUTFILE
+    load_python_build_file(python_build_file)
+    return None
+
+
+def register_actions_from_configuration(
+    configuration: Byggfile, is_restarted_with_env: str | None
+):
+    logger.info(
+        "Registering actions from configuration for '%s'", is_restarted_with_env
+    )
+    for action_name, action in configuration.actions.items():
+        if TYPE_CHECKING:
+            assert not isinstance(action, str)
+
+        if is_restarted_with_env and action.environment != is_restarted_with_env:
+            logger.info(
+                "Skipping action '%s' for environment '%s', it belongs to '%s'",
+                action_name,
+                is_restarted_with_env,
+                action.environment,
+            )
+            continue
+
+        logger.info("Registering action '%s'", action_name)
+        logger.debug("Action: %s", action)
+        shell_command = (
+            create_shell_command(action.shell, action.message) if action.shell else None
+        )
+        Action(
+            action_name,
+            # Use the shell command as fallback for the description
+            description=action.description or f"`{action.shell}`",
+            is_entrypoint=bool(action.is_entrypoint),
+            inputs=action.inputs,
+            outputs=action.outputs,
+            dependencies=action.dependencies,
+            command=shell_command,
+        )
+
+
+def load_python_build_file(build_file: str):
+    # modify load path to make the current directory importable
+    preamble = """\
+        import os
+        import sys
+        sys.path.insert(0, str(os.path.realpath('.')))
+
+        """
+
+    if os.path.isfile(build_file):
+        with open(build_file, "r") as f:
+            exec(textwrap.dedent(preamble) + f.read(), globals())

commit ae56521d2c78666d515dd60b98ba0d7671dc2502

Fix so that running multiple actions works again
Also add test for running multiple actions.

Regression from e2ff1967149741bc5b0f1bf53660ae754c1e80c6.

src/bygg/cmd/dispatcher.py
@@ -210,10 +210,7 @@ def parent_dispatcher(
                 f"Error: The following action could not be found: {TS.BOLD}'{action}'{TS.NOBOLD}."
             )
             sys.exit(1)
-        sys.exit(0)
-
-    assert not "this point should not be reached"
-    sys.exit(1)
+    sys.exit(0)
 
 
 DoerType: TypeAlias = Callable[[ByggContext, str], tuple[SubProcessIpcData, bool]]

tests/__snapshots__/test_main.ambr
@@ -1,4 +1,18 @@
 # serializer version: 1
+# name: test_build_multiple_actions
+  '''
+  
+  ✓ do something more     : I did something more
+  ✓ ls                    : 
+  ✓ shorthand action yaml, with spaces : 
+  ✓ shorthand_action_yaml : 
+  ✓ touch a file          : I touched a file
+  🛈 Building action 'shorthand action yaml, with spaces':
+  🛈 Building action 'shorthand_action_yaml':
+  🛈 Building action 'touch a file':
+  🛈 Entering directory 'examples/taskrunner'
+  '''
+# ---
 # name: test_dump_schema
   '''
   {

tests/test_main.py
@@ -151,6 +151,29 @@ def test_non_existing_action(snapshot, clean_bygg_tree, example):
     assert process.stdout == snapshot
 
 
+def test_build_multiple_actions(snapshot, clean_bygg_tree):
+    process = subprocess.run(
+        [
+            "bygg",
+            "-C",
+            examples_dir / "taskrunner",
+            "shorthand_action_yaml",
+            "touch a file",
+            "shorthand action yaml, with spaces",
+        ],
+        cwd=clean_bygg_tree,
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert process.returncode == 0
+    cleaned_result = [
+        line
+        for line in process.stdout.split("\n")
+        if "Starting process" not in line and "completed in" not in line
+    ]
+    assert "\n".join(sorted(cleaned_result)) == snapshot
+
+
 @pytest.mark.parametrize("example", examples, ids=lambda x: x.name)
 def test_reset_remove_environments(
     snapshot, clean_bygg_tree, example: ExampleParameters