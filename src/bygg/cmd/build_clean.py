e83aa4d77efbd90e192770f01f7fbe8199eaf514
Rikard Gillemyr <rikard.gillemyr@gmail.com>
2025-04-20 15:32:54 +0200

    Have build and clean take singular actions
    The build and clean commands are no longer called with lists of actions
    to work on, so simplify them to only take an action name instead.

--- src/bygg/cmd/build_clean.py
+++ src/bygg/cmd/build_clean.py
@@ -12,7 +12,7 @@ from bygg.output.status_display import failed_checks, output_check_results
 
 def build(
     ctx: ByggContext,
-    actions: list[str],
+    action: str,
     job_count: int | None,
     always_make: bool,
     check: bool,
@@ -34,26 +34,23 @@ def build(
     try:
         max_workers = get_job_count_limit() if job_count is None else job_count
 
-        for action in actions:
-            t1 = time.time()
-            output_info(f"Building action '{action}':")
+        t1 = time.time()
+        output_info(f"Building action '{action}':")
 
-            ctx.scheduler.start_run(
-                action,
-                always_make=always_make,
-                check=check,
-            )
-            status = ctx.runner.start(max_workers)
-            ctx.scheduler.shutdown()
+        ctx.scheduler.start_run(
+            action,
+            always_make=always_make,
+            check=check,
+        )
+        status = ctx.runner.start(max_workers)
+        ctx.scheduler.shutdown()
 
-            if status:
-                output_ok(f"Action '{action}' completed in {time.time() - t1:.2f} s.")
-            else:
-                output_error(
-                    f"Action '{action}' failed after {time.time() - t1:.2f} s."
-                )
-                output_job_logs(ctx.runner.failed_jobs)
-                return False
+        if status:
+            output_ok(f"Action '{action}' completed in {time.time() - t1:.2f} s.")
+        else:
+            output_error(f"Action '{action}' failed after {time.time() - t1:.2f} s.")
+            output_job_logs(ctx.runner.failed_jobs)
+            return False
 
     except KeyboardInterrupt:
         output_warning("\nBuild was interrupted by user.")
@@ -70,26 +67,25 @@ def build(
     return True
 
 
-def clean(ctx: ByggContext, actions: list[str]):
+def clean(ctx: ByggContext, action: str):
     try:
-        for action in actions:
-            output_info(f"Cleaning action '{action}':")
-            ctx.scheduler.prepare_run(action)
-            for job_name in ctx.scheduler.job_graph.get_all_jobs():
-                job = ctx.scheduler.build_actions.get(job_name, None)
-                if job is None:
-                    continue
-                for output in job.outputs:
-                    try:
-                        s = os.stat(output)
-                        if stat.S_ISREG(s.st_mode):
-                            os.unlink(output)
-                        elif stat.S_ISDIR(s.st_mode):
-                            output_info(f"Removing directory: {output}")
-                            shutil.rmtree(output)
-                    except FileNotFoundError:
-                        pass
-            ctx.scheduler.shutdown()
+        output_info(f"Cleaning action '{action}':")
+        ctx.scheduler.prepare_run(action)
+        for job_name in ctx.scheduler.job_graph.get_all_jobs():
+            job = ctx.scheduler.build_actions.get(job_name, None)
+            if job is None:
+                continue
+            for output in job.outputs:
+                try:
+                    s = os.stat(output)
+                    if stat.S_ISREG(s.st_mode):
+                        os.unlink(output)
+                    elif stat.S_ISDIR(s.st_mode):
+                        output_info(f"Removing directory: {output}")
+                        shutil.rmtree(output)
+                except FileNotFoundError:
+                    pass
+        ctx.scheduler.shutdown()
     except KeyboardInterrupt:
         output_warning("Build was interrupted by user.")
         return False
